b6c54cc1afb23a02d98ddab4d2d8d91b8bedc4da
chauhoang30 <86766653+chauhoang30@users.noreply.github.com>
2021-07-01 14:19:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Anh ơiiii!',
+    introDesc: ` Một chút tâm tư của cô gái nhỏ dành cho anh '
+     btnIntro: 'hihi',
+    title: ' Yêu em không?👻',
+    desc: 'Nắng mưa để dành cho trời
+           Riêng anh thì phải để dành cho em ',
+    btnYes: 'Yêu ơi là yêu ',
+    btnNo: 'Em nằm mơ à 🙄',
+    question:'Lí do thích em?🙆',
+    btnReply: 'Gửi cho em ',
+    reply: 'hi',
+    mess: 'Thi tốt nha ',
+    messDesc: 'Chúc tình yêu của em thi tốt mong vạn sự may mắn sẽ đến với anh.',
+    btnAccept: 'Dạ cô bé',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
